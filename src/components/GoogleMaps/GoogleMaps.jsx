9cd080518c77dbbaea3b6bf98387fccfbe2fe5e5
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-11-12 19:01:15 -0500

    All Google APIs in GoogleMaps component working.

--- src/components/GoogleMaps/GoogleMaps.jsx
+++ src/components/GoogleMaps/GoogleMaps.jsx
@@ -9,9 +9,28 @@ class GoogleMaps extends Component {
         this.state = { 
             lat: '',
             lng: '',
-            nearHosp: []
+            nearHosp: [],
+            showingInfoWindow: true,  
+            activeMarker: {},          
+            selectedPlace: {}
          }
     }
+
+    onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+    onClose = props => {
+        if (this.state.showingInfoWindow) {
+        this.setState({
+            showingInfoWindow: false,
+            activeMarker: null
+        });
+        }
+    };
 
     componentDidMount = () => {
         this.getLatLng()
@@ -59,28 +78,50 @@ class GoogleMaps extends Component {
         };
         
         service.nearbySearch(request, (results, status) => {
-            
-            console.log(results);
-            this.updateNearHospitals(results)
-            
+            if(status == google.maps.places.PlacesServiceStatus.OK){
+                console.log(results);
+                this.updateNearHospitals(results)
+            }
         })
         
     }
 
+    fetchLngLat = async (hospitalAddress) => {
+        Geocode.setApiKey(`AIzaSyCku9xApttpFw0Hkugb93TcnWJJSEvKKJE`);
+        Geocode.setRegion('en')
+        let response = await Geocode.fromAddress(hospitalAddress)  
+        console.log(response)     
+        return {lat: response.results[0].geometry.location.lat, lng: response.results[0].geometry.location.lng, name: response.name}  
+        
+    }
+
     updateNearHospitals = (hospitals) => {
-        this.setState({
-            nearHosp: hospitals
-        })
+        let coords = []
+        for(let i=0;i<hospitals.length;i++){
+            coords.push(this.fetchLngLat(hospitals[i].vicinity))
+        }  
+        Promise.all(coords)
+            .then((result) => {
+                this.setState({
+                    nearHosp: result
+                })
+            })     
     }
 
     render() { 
         
         console.log(this.state)
         return ( 
-            <Map onDragend={this.fetchPlaces} onReady={this.fetchPlaces} center={{lat: this.state.lat, lng: this.state.lng}} containerStyle={this.containerStyle} google={this.props.google} zoom={10}>
-                <Marker position={{lat: this.state.lat, lng: this.state.lng}} onClick={this.onMarkerClick} name={'Current location'}/>
-                
-                
+            <Map onZoomChanged={this.fetchPlaces} onReady={this.fetchPlaces} center={{lat: this.state.lat, lng: this.state.lng}} containerStyle={this.containerStyle} google={this.props.google} zoom={14}>
+                <Marker onClick={this.onMarkerClick} position={{lat: this.state.lat, lng: this.state.lng}} name={'Current location'}/>
+                <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow} onClose={this.onClose}>
+                    <div>
+                        <h3>{this.props.streetaddress}</h3>
+                    </div>
+                </InfoWindow>
+                {this.state.nearHosp.map((element) => {
+                    return <Marker position={{lat: element.lat,lng: element.lng}}/>
+                })}
                 
             </Map>
          );
